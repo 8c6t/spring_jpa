b89e9f7eab9a395ed87caa1d1d04a73575d21592
8c6t <nkw8620@gmail.com>
2019-11-17 17:57:24 +0900

    1.11 스프링 데이터 JPA 원리

--- ch01/demospringdata/src/main/java/com/hachicore/demospringdata/DemospringdataApplication.java
+++ ch01/demospringdata/src/main/java/com/hachicore/demospringdata/DemospringdataApplication.java
@@ -2,8 +2,11 @@ package com.hachicore.demospringdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+// @EnableJpaRepositories
+@Import(HachicoreRegistrar.class)
 public class DemospringdataApplication {
 
     public static void main(String[] args) {
--- ch01/demospringdata/src/main/java/com/hachicore/demospringdata/JpaRunner.java
+++ ch01/demospringdata/src/main/java/com/hachicore/demospringdata/JpaRunner.java
@@ -1,51 +1,24 @@
 package com.hachicore.demospringdata;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
-
 @Component
 @Transactional
+@RequiredArgsConstructor
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager em;
+    private final PostRepository postRepository;
+    private final Hachicore hachicore;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // typedQuery();
-        // criteria();
-        nativeQuery();
-    }
-
-    private void nativeQuery() {
-        List<Post> posts = em.createNativeQuery("SELECT * FROM Post", Post.class).getResultList();
-        posts.forEach(System.out::println);
-    }
-
-    private void criteria() {
-        CriteriaBuilder builder = em.getCriteriaBuilder();
-        CriteriaQuery<Post> query = builder.createQuery(Post.class);
-        Root<Post> root = query.from(Post.class);
-        query.select(root);
-
-        List<Post> posts = em.createQuery(query).getResultList();
-        posts.forEach(System.out::println);
-    }
-
-    private void typedQuery() {
-        TypedQuery<Post> query = em.createQuery("SELECT p FROM Post p", Post.class);
-        List<Post> posts = query.getResultList();
-        posts.forEach(System.out::println);
+        // postRepository.findAll().forEach(System.out::println);
+        System.out.println("=================");
+        System.out.println(hachicore.getName());
     }
 
 }
