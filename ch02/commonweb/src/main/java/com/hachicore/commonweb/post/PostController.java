810789745a7153e24cdf42fc3ee1eee12cf609e4
8c6t <nkw8620@gmail.com>
2019-11-22 17:20:47 +0900

    2.13 스프링 데이터 Common 12. 웹 기능 2부 DomainClassConverter

---
+++ ch02/commonweb/src/main/java/com/hachicore/commonweb/post/PostController.java
@@ -0,0 +1,19 @@
+package com.hachicore.commonweb.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable("id") Post post) {
+        return post.getTitle();
+    }
+
+}
