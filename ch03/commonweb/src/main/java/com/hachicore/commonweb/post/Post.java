be21eccefae25e47414002e15485f9b5443fffde
8c6t <nkw8620@gmail.com>
2019-11-23 16:17:48 +0900

    3.1 스프링 데이터 JPA 1. JpaRepository

--- ch03/commonweb/src/main/java/com/hachicore/commonweb/post/Post.java
+++ ch03/commonweb/src/main/java/com/hachicore/commonweb/post/Post.java
@@ -3,14 +3,17 @@ package com.hachicore.commonweb.post;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Getter @Setter
 @Entity
 public class Post {
 
-    @Id @GeneratedValue
+    @Id
     private Long id;
 
     private String title;
