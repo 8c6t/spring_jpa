f88d983fc273e589d87e104aaafd8aafac17c7da
8c6t <nkw8620@gmail.com>
2019-11-23 15:29:33 +0900

    2.15 스프링 데이터 Common 14. 웹 기능 4부 HATEOAS

--- ch02/commonweb/src/main/java/com/hachicore/commonweb/post/PostController.java
+++ ch02/commonweb/src/main/java/com/hachicore/commonweb/post/PostController.java
@@ -3,6 +3,9 @@ package com.hachicore.commonweb.post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +22,9 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public Page<Post> getPosts(Pageable pageable) {
-        return posts.findAll(pageable);
+    public PagedResources<Resource<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        Page<Post> all = posts.findAll(pageable);
+        return assembler.toResource(all);
     }
 
 }
