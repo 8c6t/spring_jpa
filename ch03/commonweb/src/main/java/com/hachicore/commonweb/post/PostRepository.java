bf1ccc583db161d29c80ebae18cd57a0c841c4bc
8c6t <nkw8620@gmail.com>
2019-11-24 16:09:16 +0900

    3.5 스프링 데이터 JPA 5. Named Parameter와 SpEL

--- ch03/commonweb/src/main/java/com/hachicore/commonweb/post/PostRepository.java
+++ ch03/commonweb/src/main/java/com/hachicore/commonweb/post/PostRepository.java
@@ -3,6 +3,7 @@ package com.hachicore.commonweb.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,8 +11,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByTitleStartsWith(String title);
 
-    @Query(value = "SELECT p from Post p WHERE p.title = ?1")
+    @Query(value = "SELECT p from #{#entityName} p WHERE p.title = :title")
     // @Query(value = "SELECT p, p.title AS pTitle from Post p WHERE p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+    List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
 }
