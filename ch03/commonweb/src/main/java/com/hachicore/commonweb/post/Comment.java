a7cb8b1d04d0905173e6f131e206072d2e966448
8c6t <nkw8620@gmail.com>
2019-11-24 17:28:43 +0900

    3.7 스프링 데이터 JPA 7. EntityGraph

---
+++ ch03/commonweb/src/main/java/com/hachicore/commonweb/post/Comment.java
@@ -0,0 +1,21 @@
+package com.hachicore.commonweb.post;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// @NamedEntityGraph(name = "Comment.post", attributeNodes = @NamedAttributeNode("post"))
+@Entity
+@Getter @Setter
+public class Comment {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+}
---
+++ ch03/commonweb/src/test/java/com/hachicore/commonweb/post/CommentRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.hachicore.commonweb.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void getComment() throws Exception {
+        commentRepository.getById(1L);
+        System.out.println("==============================");
+        commentRepository.findById(1L);
+    }
+
+}
